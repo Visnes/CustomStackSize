7867166593f59f58bf4616025e68319652aa2a3f
Kim Visnes <15141314+Visnes@users.noreply.github.com>
2025-11-07 19:17:37 +0100

    Add files via upload

---
+++ CustomStackSize_1.0.0/settings.lua
@@ -0,0 +1,42 @@
+data:extend({
+  {
+    type = "int-setting",
+    name = "stack-size-brick",
+    setting_type = "startup",
+    default_value = 100,
+    minimum_value = 1,
+    maximum_value = 100000
+  },
+  {
+    type = "int-setting",
+    name = "stack-size-concrete",
+    setting_type = "startup",
+    default_value = 100,
+    minimum_value = 1,
+    maximum_value = 100000
+  },
+  {
+    type = "int-setting",
+    name = "stack-size-refined-concrete",
+    setting_type = "startup",
+    default_value = 100,
+    minimum_value = 1,
+    maximum_value = 100000
+  },
+  {
+    type = "int-setting",
+    name = "stack-size-landfill",
+    setting_type = "startup",
+    default_value = 100,
+    minimum_value = 1,
+    maximum_value = 100000
+  },
+  {
+    type = "int-setting",
+    name = "stack-size-wood",
+    setting_type = "startup",
+    default_value = 100,
+    minimum_value = 1,
+    maximum_value = 100000
+  }
+})
